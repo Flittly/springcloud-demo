d5e8ca2d8718754679bc0e9eb2cc3c07df1e05c2
Flittly <2605948957@qq.com>
2026-08-18 11:17:37 +0800

    feat(order): 集成Feign客户端配置并优化日志记录
    - 在application.yml中添加feign配置文件包含

--- services/service-product/src/main/java/com/flittly/service/impl/ProductServiceImpl.java
+++ services/service-product/src/main/java/com/flittly/service/impl/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import com.flittly.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -15,6 +16,12 @@ public class ProductServiceImpl implements ProductService {
         product.setPrice(new BigDecimal("99"));
         product.setProductName("苹果-" + productId);
         product.setNum(2);
+
+        try {
+            TimeUnit.SECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         return product;
     }
